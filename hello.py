00f483d538e4b9f17c659086eb03629a788490cc
Moaz Taha <moaaztaha7@gmail.com>
2024-01-15 23:19:32 +0200

    scaffold

---
+++ hello.py
@@ -0,0 +1,22 @@
+"""
+This is a module docstring for the hello.
+
+It only has a function to add 2 numbers now.
+"""
+
+
+def add(num1, num2):
+    """
+    Adds two numbers together and returns the sum.
+
+    Parameters:
+        num1 (int): The first number.
+        num2 (int): The second number.
+
+    Returns:
+        int: The sum of the two numbers.
+    """
+    return num1 + num2
+
+
+print(add(1, 2))
